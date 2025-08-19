6d302b3c3ba140d8c0063f5c4a67556e4c92c697
Ryan Wez <ryanwez25@gmail.com>
2025-08-19 03:26:48 +0000

    Critical: "PrismaClientInitializationError" Persists. Let's Perform a Ha

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,5 +1,5 @@
-import { PrismaClient } from '@prisma/client';
-
 // When using Prisma's Data Proxy, it's recommended to instantiate a new client for each request
 // rather than using a global singleton. The Data Proxy handles connection pooling.
+import { PrismaClient } from '@prisma/client/edge';
+
 export const prisma = new PrismaClient();
