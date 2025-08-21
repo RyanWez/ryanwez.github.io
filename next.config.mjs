a70846d71060c01708cc80c67d249fb995661c8a
Ryan Wez <ryanwez25@gmail.com>
2025-08-21 02:18:11 +0000

    The NextJS app server is having trouble starting. Please identify what c

--- next.config.mjs
+++ next.config.mjs
@@ -1,18 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-<<<<<<< HEAD
   output: 'export',
-  images: {
-    unoptimized: true,
-  }
-=======
-  reactStrictMode: true,
-  output: 'export',
-  basePath: '',
   images: {
     unoptimized: true,
   },
->>>>>>> origin/main
 };
 
 export default nextConfig;
